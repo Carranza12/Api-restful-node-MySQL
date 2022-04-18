f542c809f83ddc6d86f025d3d7644b15be926458
Francisco Javier Carranza Esquivel <60407692+Carranza12@users.noreply.github.com>
2022-04-18 10:24:36 -0500

    estructurando readme

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
-# node-iyd2da
+# API-REST Empleados CRUD -NodeJs-MySQL
+Hola, esta es una aplicacion con el marco de trabajo de Express con node js y en la base de datos usando MySQL, es un CRUD con las operaciones basicas para guardar registros en la base de datos, a lo largo de la documentacion probaremos la app con Postman.
+<br>
+<br>
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-iyd2da)
+# Configuracion del entorno de trabajo
+Para poder ejecutar este proyecto es necesario tener instalado:<br><br>
+- MySQL - guia de instalacion: http://gieta.etsisi.upm.es/wp-content/uploads/2020/02/GuiaInstalacion_MySql_Community_8.0.19.pdf <br>
+- Node js - Link de descarga: https://nodejs.org/es/ <br>
+# Requisitos del proyecto Spring Boot: <br>
+- MySQL 8.0 <br>
+- Node js <br>
+- Postman <br>
+# Configura la Base de datos
+Es necesario tener una base de datos con el nombre de "company" , despues ejecutar los scripts del archivo "employees.sql".
+```
+CREATE DATABASE company;
+
+USE company;
+
+CREATE TABLE employees (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(11) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+INSERT INTO employees VALUES 
+(1,'Juan Carmona',2200),
+(2,'Alexis Hernandez',2500),
+(3,'Alejandro Valenzuela',4670);
+
+
+SELECT * FROM company.employees;
+
+```
+<br><br>
+
+# Instala las dependecias de Node js para el proyecto
+Necesitas ubicarte en la carpeta raiz y ejecutar el siguiente comando:
+```
+$ npm i
+
+```
+# Ejecuta el servidor local
+una vez ya instalado todo lo necesario y ya clonado el proyecto ejecuta el servidor con el siguiente comando:
+```
+$ npm run dev
+```
+
+# Probando API con POSTMAN
+a continuacion probaremos todos los metodos HTTP de nuestra API.
+
+# GET: Listar todos los empleados
+![Listar todos los empleados](https://github.com/Carranza12/API-REST-SpringBoot-MySQL/blob/master/assets-repo/get-clientes.PNG)
+
+# GET: listar un empleado por su id
+![Listar un cliente](https://github.com/Carranza12/API-REST-SpringBoot-MySQL/blob/master/assets-repo/get-cliente.PNG)
+# POST: Guardar un empleado
+![guardar un empleado](https://github.com/Carranza12/API-REST-SpringBoot-MySQL/blob/master/assets-repo/post-cliente.PNG)
+# PUT: Actualizar los datos de un empleado
+![actualizar un cliente](https://github.com/Carranza12/API-REST-SpringBoot-MySQL/blob/master/assets-repo/put-cliente.PNG)
+# DELETE: Eliminar un empleado de la BD
+![eliminar un cliente](https://github.com/Carranza12/API-REST-SpringBoot-MySQL/blob/master/assets-repo/delete-cliente.PNG)
--- src/routes/employeesRoute.js
+++ src/routes/employeesRoute.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mysqlConnection = require('../database.js');
+const mysqlConnection = require("../database.js");
 
-router.get('', (req, res) => {
-  mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
+router.get("", (req, res) => {
+  mysqlConnection.query("SELECT * FROM employees", (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -11,63 +11,89 @@ router.get('', (req, res) => {
     }
   });
 });
-router.get('/:id',(req,res)=>{
-  const {id}=req.params;
-  mysqlConnection.query('SELECT * FROM employees WHERE id=?',[id],(err,rows,fields)=>{
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query(
+    "SELECT * FROM employees WHERE id=?",
+    [id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
     }
-  })
-})
-router.post('',(req,res)=>{
-  if(!req.body.name){
-    res.status(405)
-    res.send({name:"debe de poner un nombre"})
+  );
+});
+router.post("", (req, res) => {
+  if (!req.body.name) {
+    res.status(405);
+    res.send({ name: "debe de poner un nombre" });
   }
-  if(!req.body.salary){
-    res.status(405)
-    res.send({salary:"debe de poner un salario"})
-  }else{
-    const employeeData={
-      name:req.body.name,
-      salary:req.body.salary
-    }
-  
-  
-  mysqlConnection.query('INSERT INTO `company`.`employees` (`name`, `salary`) VALUES (?, ?)',[employeeData.name,employeeData.salary],(err,rows,fields)=>{
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
-    }
-  })
+  if (!req.body.salary) {
+    res.status(405);
+    res.send({ salary: "debe de poner un salario" });
+  } else {
+    const employeeData = {
+      name: req.body.name,
+      salary: req.body.salary,
+    };
+
+    mysqlConnection.query(
+      "INSERT INTO `company`.`employees` (`name`, `salary`) VALUES (?, ?)",
+      [employeeData.name, employeeData.salary],
+      (err, rows, fields) => {
+        if (!err) {
+          res.status(200);
+          res.send("Empleado dado de alta en el sistema!");
+        } else {
+          console.log(err);
+        }
+      }
+    );
   }
-    
-})
-router.put('/:id',(req,res)=>{
-  const employeeNewData={
-    id:req.params.id,
-    name:req.body.name,
-    salary:req.body.salary
+});
+router.put("/:id", (req, res) => {
+  if (!req.body.name) {
+    res.status(405);
+    res.send({ name: "debe de poner un nombre" });
   }
-  mysqlConnection.query('UPDATE `company`.`employees` SET `name` = ?, `salary` = ? WHERE (`id` = ?);',[employeeNewData.name,employeeNewData.salary,employeeNewData.id],(err,rows,fields)=>{
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
+  if (!req.body.salary) {
+    res.status(405);
+    res.send({ salary: "debe de poner un salario" });
+  } else {
+    const employeeNewData = {
+      id: req.params.id,
+      name: req.body.name,
+      salary: req.body.salary,
+    };
+    mysqlConnection.query(
+      "UPDATE `company`.`employees` SET `name` = ?, `salary` = ? WHERE (`id` = ?);",
+      [employeeNewData.name, employeeNewData.salary, employeeNewData.id],
+      (err, rows, fields) => {
+        if (!err) {
+          res.status(200)
+          res.send("Empleado Actualizado correctamente!")
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  }
+});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query(
+    "DELETE FROM `company`.`employees` WHERE (`id` = ?);",
+    [id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.status(100)
+        res.send("Empleado Eliminado correctamente.")
+      } else {
+        console.log(err);
+      }
     }
-  })
-})
-router.delete('/:id',(req,res)=>{
-  const {id}=req.params
-  mysqlConnection.query('DELETE FROM `company`.`employees` WHERE (`id` = ?);',[id],(err,rows,fields)=>{
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
-    }
-  })
-})
+  );
+});
 module.exports = router;
