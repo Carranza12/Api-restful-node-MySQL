e8131a7d97202b8fb46987b18be56a4edac31476
Carranza12 <franckgta3@gmail.com>
2022-04-17 22:16:32 -0500

    1 file modified, 5 added, 1 deleted

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const employeeRoute = require('./routes/employeesRoute');
+//settings
+app.set('port', process.env.PORT || 3000);
+//middlewares
+app.use(express.json());
+
+//routes
+app.use(employeeRoute);
+//starting the server
+app.listen(app.get('port'), () => {
+  console.log('server corriendo en el puerto: ', app.get('port'));
+});
